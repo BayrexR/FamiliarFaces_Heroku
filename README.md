5dec535ef6eed80019b8ce6e2e43584894a38c5a
Bayrex <bayrexr@gmail.com>
2019-03-05 09:53:30 -0500

    Adds Flask app boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# FamiliarFaces_Heroku
-Heroku deployed flask app repo
+# Familiar Faces
+This project uses facifier to leverage machine learning to classify the emotions of image subjects. The pupose is to identify a trend in the emotional health of an individual based on a dataset of images.
---
+++ static/assets/Resources/config.py
@@ -0,0 +1,13 @@
+# SQL username and password. Default will be 'root'. Change as needed.
+
+#Localhost dev db credentials
+username = "root"
+password = "root"
+dbName = "familiar_faces_db"
+
+
+#JawsDB Prod Credentials
+# username = "ulu4bh58ufwd3iai"
+# password = "tzns6d8dpaaigshj"
+
+use_anonymous = True
