6b2710ce9bd0707b3406cfea1649c4f19995c17b
Bayrex <bayrexr@gmail.com>
2019-03-05 10:43:38 -0500

    Corrected connection variable errors.

--- static/assets/Resources/config.py
+++ static/assets/Resources/config.py
@@ -3,7 +3,7 @@
 #Localhost dev db credentials
 # username = "root"
 # password = "root"
-# dbHost = "@localhost"
+# dbHost = "localhost"
 # dbPort = 3306
 # dbName = "familiar_faces_db"
 
@@ -11,8 +11,8 @@
 #JawsDB Prod Credentials
 username = "dgb19ecqujwu8qlc"
 password = "tqhoyrxfc68ozobj"
-dbHost = "@r4919aobtbi97j46.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-dbPort = 3306
+dbHost = "r4919aobtbi97j46.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+dbPort = "3306"
 dbName = "zn89tk61j4bd12s8"
 
 use_anonymous = True
